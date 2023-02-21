5dc15f4f3c26015f6ecc0ceb40569b9d1fde1bb0
gyeomson <kgs960209@naver.com>
2023-02-21 17:02:25 +0900

    Team

---
+++ Team/src/Team/Team.java
@@ -0,0 +1,10 @@
+package Team;
+
+public class Team {
+
+	public static void main(String[] args) {
+		System.out.println("살려줘");
+
+	}
+
+}
---
+++ Team/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author kimgyeomson
+ *
+ */
+module Team {
+}
